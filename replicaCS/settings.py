688dc71fbcb4ac36a1c5e5b3b37c27deab36dcaf
erikaher <183420@ids.upchiapas.edu.mx>
2020-02-23 20:26:41 -0600

    updateSettings

--- replicaCS/settings.py
+++ replicaCS/settings.py
@@ -48,11 +48,10 @@ INSTALLED_APPS = [
 SITE_ID = 1
 
 REST_FRAMEWORK = {
-'DEFAULT_PERMISSION_CLASSES':('rest_framework.permissions.AllowAny',),
+'DEFAULT_PERMISSION_CLASSES':('rest_framework.permissions.IsAuthenticated',),
 'DEFAULT_AUTHENTICATION_CLASSES':('rest_framework.authentication.TokenAuthentication',),
 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-'PAGE_SIZE': 100,
-'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema'
+'PAGE_SIZE': 100
 }
 
 CORS_ALLOW_METHODS = (
